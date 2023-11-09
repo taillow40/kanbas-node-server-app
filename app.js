d9dbc75914952121eade8678fff82186ce23483a
SamSurface <spt7ponder@aol.com>
2023-11-09 12:28:34 -0500

    Finished Server Side

--- app.js
+++ app.js
@@ -4,23 +4,28 @@ import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
-
 const app = express();
+app.use(express.json());
 app.use(
     cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
+        credentials: true,
+        origin: process.env.FRONTEND_URL
     })
-  );
-  
+);
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    next();
+});
 
 Lab5(app);
 
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 HelloRoutes(app);
 
 
--- lab5.js
+++ lab5.js
@@ -8,21 +8,86 @@ const assignment = {
 };
 
 const todos = [
-  { id: 1, title: "Task 1", completed: false },
+  { id: 1, title: "Task 1", completed: true },
   { id: 2, title: "Task 2", completed: false },
-  { id: 3, title: "Task 3", completed: false },
+  { id: 3, title: "Task 3", completed: true },
   { id: 4, title: "Task 4", completed: false },
 ];
 
 function Lab5(app) {
-  app.get("/a5/todos", (req, res) => {
-    const { completed } = req.query;
-    if (completed !== undefined) {
-      const completedTodos = todos.filter(
-        (t) => t.completed === completed);
-      res.json(completedTodos);
+  app.get("/a5/todos/:id/title/:title", (req, res) => {
+    const { id, title } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = title;
+    res.json(todos);
+  });
+
+  app.get("/a5/todos/:id/delete", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todos.splice(todos.indexOf(todo), 1);
+    res.json(todos);
+  });
+
+  app.put("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    todo.due = req.body.due;
+    todo.completed = req.body.completed;
+    res.sendStatus(200);
+  });
+
+  app.delete("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (!todo) {
+      res.sendStatus(404);
       return;
     }
+
+    todos.splice(todos.indexOf(todo), 1);
+    // res.json(todos);
+    res.sendStatus(204);
+  });
+
+  app.post("/a5/todos", (req, res) => {
+    const newTodo = {
+      ...req.body,
+      id: new Date().getTime(),
+    };
+    todos.push(newTodo);
+    res.json(newTodo);
+  });
+
+  app.get("/a5/todos/create", (req, res) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      title: "New Task",
+      completed: false,
+    };
+    todos.push(newTodo);
+    res.json(todos);
+  });
+  app.get("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((todo) => todo.id === parseInt(id));
+    res.json(todo);
+  });
+
+  app.get("/a5/todos", (req, res) => {
+    const { completed, due, assignedTo } = req.query;
+    if (completed === "true") {
+      const completedTodos = todos.filter((todo) => todo.completed);
+      res.json(completedTodos);
+      return;
+    } else if (completed === "false") {
+      const incompletedTodos = todos.filter((todo) => !todo.completed);
+      res.json(incompletedTodos);
+      return;
+    }
+    res.json(todos);
 
   });
   app.get("/a5/todos/:id", (req, res) => {
