8c91f635582d52e7044b6fbc783656d65177ba59
Sam Desktop <spt7ponder@aol.com>
2023-12-03 19:26:01 -0500

    Added

--- hello.js
+++ hello.js
@@ -2,6 +2,9 @@ function HelloRoutes(app) {
   const hello = (req, res) => {
     res.send("Life is good!");
   };
+  const fullStack = (req, res) => {
+    res.send("Welcome to Full Stack!");
+  }
   app.get("/hello", hello);
 }
 export default HelloRoutes;
